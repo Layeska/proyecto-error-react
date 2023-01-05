fee358bb1b3a3a4ccc168a8d34dcfb353675ae77
Ana Juárez <lisa13suarez@gmail.com>
2023-01-05 14:11:37 -0600

    commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Menu from './components/menu'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Menu/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Menu.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <>
+        <div className='menu'>
+            <div className='img'>
+                Ana Juárez
+            </div>
+            <nav>
+                <ul>
+                    <li>Inicio 5</li>
+                    <li>Acerca de mi</li>
+                    <li>Proyectos</li>
+                </ul>
+            </nav>
+            <div>
+                <button>Contáctame</button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+
+export default Menu
